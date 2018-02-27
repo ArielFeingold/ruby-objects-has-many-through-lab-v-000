88434213faffd2ed5abfb35de242b5011a729f52
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 22:38:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
 
   attr_accessor :name
 
-  def initialize
+  def initialize(name)
     @name = name
     @songs = []
   end
@@ -12,7 +12,7 @@ class Artist
   end
 
   def songs
-    
+
   end
 
 
